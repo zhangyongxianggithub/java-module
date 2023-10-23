37cc02de82e71c6d75bc88cd841e0435f19aefa7
zhangyongxiang <zhangyongxiang@baidu.com>
2023-10-23 17:10:41 +0800

    update

--- expenses.readers/src/main/java/com/example/expenses/readers/file/FileReader.java
+++ expenses.readers/src/main/java/com/example/expenses/readers/file/FileReader.java
@@ -11,15 +11,10 @@ import static java.util.stream.Collectors.joining;
 public class FileReader implements Reader {
     @Override
     public String read(final String path) {
-<<<<<<< HEAD
-       // return FileReader.class.getClassLoader().getResourceAsStream("classpath:file.txt");
-        return "tezt";
-=======
         final InputStream inputStream = this.getClass()
                 .getResourceAsStream(path);
         final BufferedReader reader = new BufferedReader(
                 new InputStreamReader(inputStream));
         return reader.lines().collect(joining("\n"));
->>>>>>> b713137 (pdate)
     }
 }
